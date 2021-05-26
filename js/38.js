f46a4463c6ed19775d426cc6100bf986c32b8cc5
OlhaDudchenko <dudchenko.olha92@gmail.com>
2021-05-26 18:52:10 +0300

    delete files

--- js/38.js
+++
@@ -1,8 +0,0 @@
-const atTheOldToad = {
-  potions: ['Зелье скорости', 'Дыхание дракона', 'Каменная кожа'],
-  addPotion(potionName) {
-    // Пиши код ниже этой строки
-      this.potions.push(potionName);
-    // Пиши код выше этой строки
-  },
-};
--- js/practice.js
+++ js/practice.js
@@ -1,18 +1,49 @@
-//  'use strict';
+class User {
+  email;
 
-const animal = { eats: true };
-const dog = Object.create(animal);
-console.log("🚀 ~ file: practice.js ~ line 5 ~ dog", dog)
-dog.barks = true;
+  constructor(email) {
+    this.email = email;
+  }
 
-console.log(barks); // true
-console.log(dog.eats); // true
+  get email() {
+    return this.email;
+  }
 
+  set email(newEmail) {
+    this.email = newEmail;
+  }
+}
+class Admin extends User {
+  // Пиши код ниже этой строки
 
+  static AccessLevel = {
+    BASIC: 'basic',
+    SUPERUSER: 'superuser'
+  };
 
+  accessLevel;
+  blacklistedEmails=[];
 
+  constructor({ email, accessLevel }) {
+    super(email);
+    this.accessLevel = accessLevel;
+  }
+blacklist(email){
+ this.blacklistedEmails.push(email);}
+  
+  isBlacklisted(email){
+   return this.blacklistedEmails.includes(email);}
+  // Пиши код выше этой строки
+}
 
+const mango = new Admin({
+  email: 'mango@mail.com',
+  accessLevel: Admin.AccessLevel.SUPERUSER
+});
 
-
-
-
+console.log(mango.email); // mango@mail.com
+console.log(mango.accessLevel); // superuser
+mango.blacklist('poly@mail.com');
+console.log(mango.blacklistedEmails); // 'poly@mail.com'
+console.log(mango.isBlacklisted('mango@mail.com')); //  false
+console.log(mango.isBlacklisted('poly@mail.com')); // true 
